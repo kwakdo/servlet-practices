ce9987bd159dcc514ca36fbe6c4be0360f1a3270
kwakdo <vv05044@gmail.com>
2022-05-20 16:20:23 +0900

    servlet

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -12,11 +12,11 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
 		
 		response.setContentType("text/html; charset=UTF-8");
-		
 		PrintWriter pw = response.getWriter();
-		pw.println("<h1>안녕하세요!!!!</h1>");
+		pw.println("<h1>안녕하세요!!!!" + name + "</h1>");
 	
 	}
 
--- helloweb/src/main/webapp/tag.jsp
+++ helloweb/src/main/webapp/tag.jsp
@@ -40,7 +40,7 @@
 		문장3입니다.~~~ <br/>
 	</p>
 	<br/>
-	<a href="/helloweb/hello">hello로 가기</a>
+	<a href="/helloweb/hello?name=곽도훈">hello로 가기</a>
 	<br/>
 	<a href="form.jsp">폼으로 가기</a>
 </body>
