286aaff322a37f95bf3cf54f3451e04f1ee608f4
kwakdo <vv05044@gmail.com>
2022-05-30 10:36:37 +0900

    lc

--- helloweb/src/main/java/helloweb/LifeCycleServlet.java
+++ helloweb/src/main/java/helloweb/LifeCycleServlet.java
@@ -7,47 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LifeCycleServlet
- */
+
 public class LifeCycleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
+	
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		System.out.println("LifeCycleServlet.init(...) called");
+		super.init();
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("LifeCycleServlet.service(...) called");
+		super.service(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("LifeCycleServlet.doGet(...) called");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("LifeCycleServlet.doPost(...) called");
 		doGet(request, response);
 	}
-
+	
+	public void destroy() {
+		super.destroy();
+		System.out.println("LifeCycleServlet.destroy(...) called!!!!");
+	}
 }
